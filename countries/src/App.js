758e6b0bcc5a5e296689faac45bc6aa927ebdd33
yuvaraj <yuvaraj@coursera-something.com>
2024-05-08 19:51:58 +0530

    Countries buildout project completed

---
+++ countries/src/App.js
@@ -0,0 +1,11 @@
+import Countries from "./Countries"
+
+function App() {
+  return (
+    <div className="App">
+    <Countries />
+    </div>
+  );
+}
+
+export default App;
---
+++ countries/src/Countries.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+const Tile = ({flagUrl, name, altFlag}) => {
+
+    return ( <div style={{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems: 'center',
+        margin: '10px',
+        padding: '10px',
+        border: '1px solid black',
+        borderRadius: '8px',
+        width: '200px'
+        
+    }}>
+        <img src={flagUrl} alt={altFlag} style={{
+            width: "100px", height: '100px'
+        }} />
+
+        <h2>{name}</h2>
+    </div>
+    )
+}
+
+const Countries = () => {
+    const API_URL = 'https://restcountries.com/v3.1/all';
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch(API_URL)
+        .then((response) => response.json())
+        .then((data) => setCountries(data));
+    },[])
+    
+    return (
+        <div style={{
+            display:"flex",
+            justifyContent:'center',
+            alignItems:'center',
+            height:"100vh",
+            flexWrap: 'wrap'
+        }}>
+           {
+            countries.map((country) => (
+                <Tile key={country.cca3} flagUrl={country.flags.png} name={country.name.common} altFlag={country.flags.alt} />
+            ))
+           }
+        </div>
+    )
+}
+
+export default Countries
